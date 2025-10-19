1a47c750265103119ce5bfaa2ca549c2741bd6ad
dike1 <dike1@MacBook-Air.local>
2025-10-19 02:09:41 +0100

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
+  const [theme, setTheme] = React.useState('light')
   return (
-    <div></div>
+    <div className='dark:bg-black relative'>
+      <Navbar theme={theme} setTheme={setTheme}/>
+    </div>
   )
 }
 
